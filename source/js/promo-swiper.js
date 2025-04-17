08eee75a1e60918f68542c83a4c6440c07c28ec5
konstantin-petruschin <kostikbgsha@gmail.com>
2025-04-18 02:36:56 +0300

    стилизация и иницилизация слайдера в промо

--- source/js/promo-swiper.js
+++ source/js/promo-swiper.js
@@ -6,17 +6,24 @@ export const initPromoSlider = () => {
   new Swiper('.promo-swiper', {
     modules: [Pagination],
     slidesPerView: 1,
-    spaceBetween: 40,
-    loop: true,
+    initialSlide: 0,
     watchOverflow: true,
+    centeredSlides: true,
+    loop: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
       type: 'bullets',
-  },
-
-  observer: true,
-  observeParents: true,
+      renderBullet: function (index, className) {
+        // Исправлено на возврат HTML-строки
+        return `<button class="${className}" tabindex="0" aria-label="Слайд ${index + 1}"></button>`;
+      },
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+   
     breakpoints: {
       1440: {
         allowTouchMove: false,
